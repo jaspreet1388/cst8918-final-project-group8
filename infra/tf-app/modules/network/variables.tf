0c009bee920bd72ca07f711d4ba966d16b7db4ce
jaspreet1388 <chha0038@algonquinlive.com>
2025-08-10 00:53:51 -0400

    Update variables.tf

--- infra/tf-app/modules/network/variables.tf
+++ infra/tf-app/modules/network/variables.tf
@@ -7,11 +7,11 @@ variable "location" {
 variable "network_rg_name" {
   description = "The name of the resource group for networking"
   type        = string
-  default     = "cst8918-final-project-group-3"
+  default     = "cst8918-final-project-group-8"
 }
 
 variable "main_vnet_name" {
   description = "The name of the main virtual network"
   type        = string
-  default     = "grp3-vnet"
+  default     = "grp8-vnet"
 }
