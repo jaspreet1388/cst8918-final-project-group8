3264c13e2a790bd1b1d9e93fdb5359c2121a4d1c
jaspreet1388 <chha0038@algonquinlive.com>
2025-08-10 02:16:00 -0400

    Update variables.tf

--- infra/tf-app/modules/acr/variables.tf
+++ infra/tf-app/modules/acr/variables.tf
@@ -1,7 +1,7 @@
-variable "location" {}
-variable "resource_group_name" {}
-variable "acr_name" {}
-variable "redis_test_name" {}
-variable "redis_prod_name" {}
-variable "test_aks_identity_id" {}
-variable "prod_aks_identity_id" {}
+variable "location" {type = string}
+variable "resource_group_name" {type = string}
+variable "acr_name" {type = string}
+variable "redis_test_name" {type = string}
+variable "redis_prod_name" {type = string}
+variable "test_aks_identity_id" {type = string}
+variable "prod_aks_identity_id" {type = string}
